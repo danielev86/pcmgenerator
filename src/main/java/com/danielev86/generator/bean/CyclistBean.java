c5eb60898166584616c2d2b26e4c0ea6afa6d3f1
daniele <danieleverr@gmail.com>
2018-08-05 18:23:02 +0200

    Add Cyclist Bean and gitignore gile

---
+++ src/main/java/com/danielev86/generator/bean/CyclistBean.java
@@ -0,0 +1,133 @@
+package com.danielev86.generator.bean;
+
+public class CyclistBean {
+
+	private String firstName;
+	private String lastName;
+	private Integer characPlan;
+	private Integer characMountain;
+	private Integer characDownHilling;
+	private Integer characCobble;
+	private Integer characTimetrial;
+	private Integer characPrologue;
+	private Integer characAcceleration;
+	private Integer characEndurance;
+	private Integer characResistance;
+	private Integer characRecuperation;
+	private Integer characHill;
+
+	private Integer characBaroudeur;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getCharacPlan() {
+		return characPlan;
+	}
+
+	public void setCharacPlan(Integer characPlan) {
+		this.characPlan = characPlan;
+	}
+
+	public Integer getCharacMountain() {
+		return characMountain;
+	}
+
+	public void setCharacMountain(Integer characMountain) {
+		this.characMountain = characMountain;
+	}
+
+	public Integer getCharacDownHilling() {
+		return characDownHilling;
+	}
+
+	public void setCharacDownHilling(Integer characDownHilling) {
+		this.characDownHilling = characDownHilling;
+	}
+
+	public Integer getCharacCobble() {
+		return characCobble;
+	}
+
+	public void setCharacCobble(Integer characCobble) {
+		this.characCobble = characCobble;
+	}
+
+	public Integer getCharacTimetrial() {
+		return characTimetrial;
+	}
+
+	public void setCharacTimetrial(Integer characTimetrial) {
+		this.characTimetrial = characTimetrial;
+	}
+
+	public Integer getCharacPrologue() {
+		return characPrologue;
+	}
+
+	public void setCharacPrologue(Integer characPrologue) {
+		this.characPrologue = characPrologue;
+	}
+
+	public Integer getCharacAcceleration() {
+		return characAcceleration;
+	}
+
+	public void setCharacAcceleration(Integer characAcceleration) {
+		this.characAcceleration = characAcceleration;
+	}
+
+	public Integer getCharacEndurance() {
+		return characEndurance;
+	}
+
+	public void setCharacEndurance(Integer characEndurance) {
+		this.characEndurance = characEndurance;
+	}
+
+	public Integer getCharacResistance() {
+		return characResistance;
+	}
+
+	public void setCharacResistance(Integer characResistance) {
+		this.characResistance = characResistance;
+	}
+
+	public Integer getCharacRecuperation() {
+		return characRecuperation;
+	}
+
+	public void setCharacRecuperation(Integer characRecuperation) {
+		this.characRecuperation = characRecuperation;
+	}
+
+	public Integer getCharacHill() {
+		return characHill;
+	}
+
+	public void setCharacHill(Integer characHill) {
+		this.characHill = characHill;
+	}
+
+	public Integer getCharacBaroudeur() {
+		return characBaroudeur;
+	}
+
+	public void setCharacBaroudeur(Integer characBaroudeur) {
+		this.characBaroudeur = characBaroudeur;
+	}
+
+}
